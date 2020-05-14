c56a88191aa254621d2f31041dbb6509e1eac1cb
Zhenyu <35570683+ZhenyuTan@users.noreply.github.com>
2020-05-14 16:18:07 -0400

    project1

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,16 @@
 #include "cards.h"
+#include <stdio.h>
 
 int main(void) {
+  card_t c ;
+  c.value=4;
+  c.suit =0;
 
+
+
+  for (int i=0;i<52;i++){
+    c=card_from_num(i);
+    print_card(c);
+    printf("\n");
+  }
 }
