3c55ea7bb85bd284142bc11a30ce5aeafa2d8522
Zhenyu <35570683+ZhenyuTan@users.noreply.github.com>
2020-06-02 17:38:35 -0400

    hw29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,9 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * outname = malloc((strlen(inputName)+10)*sizeof(* outname));
+  strcpy(outname, inputName);
+  strcat(outname, ".counts");
+  return outname;
 }
+
