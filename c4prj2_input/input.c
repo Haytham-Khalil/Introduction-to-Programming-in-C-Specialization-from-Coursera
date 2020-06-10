9b48702dbfadf26e1cd323162c0d6368b93e8e01
Zhenyu <35570683+ZhenyuTan@users.noreply.github.com>
2020-06-10 17:28:05 -0400

    fix bugs

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -1,10 +1,10 @@
-
 #include<string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
 #include "cards.h"
+#include<ctype.h>
 #include "future.h"
 size_t strToInt(char * str){
   size_t len = strlen(str);
